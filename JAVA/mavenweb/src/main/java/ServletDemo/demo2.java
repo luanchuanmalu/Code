d18a79d0ca02a877b1deeecb8493c5c01e323dec
luanchuanmalu <jian_hua_yang@163.com>
2017-06-06 08:15:48 +0800

    0606

--- JAVA/mavenweb/src/main/java/ServletDemo/demo2.java
+++ JAVA/mavenweb/src/main/java/ServletDemo/demo2.java
@@ -1,24 +1,21 @@
 package ServletDemo;
 
-import java.io.*;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class HelloWorldServlet
+ * Servlet implementation class demo2
  */
-public class HelloWorldServlet extends HttpServlet {
+public class demo2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HelloWorldServlet() {
+    public demo2() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,18 +25,8 @@ public class HelloWorldServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.getOutputStream().write("servletDemo2".getBytes());
 		
-		ServletContext context = this.getServletContext();//获取ServletContext对象
-		
-		
-        //获取整个web站点的初始化参数
-        //String contextInitParam = context.getInitParameter("url");
-        //response.getWriter().print(contextInitParam);
-        
-        RequestDispatcher rd = context.getRequestDispatcher("/d2");//获取请求转发对象(RequestDispatcher)
-		rd.forward(request, response);//调用forward方法实现请求转发
-        
-        
 	}
 
 	/**
